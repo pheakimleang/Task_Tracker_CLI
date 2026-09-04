f8ff1f70bf87bcb490d95175d69a37bccb18cfe1
pheakimleang <pheakimleang13@gmail.com>
2026-09-04 19:33:41 +0700

    testing dateTimeComparison

--- Testing/IsAfter.java
+++ Testing/IsAfter.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 public class IsAfter {
     public static void main(String[] args){
+        /*
+        How to use the isAfter();
+        #In Java, isAfter() is a built-in method used to check if a specific date,
+            time, or instant occurs chronologically after another specified date or time.
+            It evaluates the comparison and returns a boolean value (true or false)
+        */
+
         //let's say my app release in 2026-09-05
         LocalDate releaseDate = LocalDate.parse("2026-09-05");
         //and today is 2026-09-04
